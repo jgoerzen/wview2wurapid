69d7e6e2dc9774aef7862fac45e16125111b0ef3
John Goerzen <jgoerzen@complete.org>
2011-08-24 01:09:08 -0500

    Fix another typo

--- wview2wurapid.sh
+++ wview2wurapid.sh
@@ -37,7 +37,7 @@ runner () {
 }
 
 while true; do
-   logger -t "$SOFTARE/$$/$DATAFEED" < "$ERRORFIFO" &
+   logger -t "$SOFTWARE/$$/$DATAFEED" < "$ERRORFIFO" &
    LOGPID="$!"
    ./wudatafeedClient 2> "$ERRORFIFO" | runner
    sleep 2
